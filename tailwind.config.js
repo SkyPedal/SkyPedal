b61e9df50082cffe620363765ee0b17d144034cc
William Moolman <moolman.william@gmail.com>
2024-07-29 14:33:53 +0100

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
