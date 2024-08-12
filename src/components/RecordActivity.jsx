4a508ee19aad9dc9a5549d0c88dfcd8d1daafbab
William Moolman <moolman.william@gmail.com>
2024-08-12 15:34:43 +0100

    feat: add dynamic data import +styling

--- src/components/RecordActivity.jsx
+++ src/components/RecordActivity.jsx
@@ -5,24 +5,12 @@ import useApi from "../repos/api";
 import { useAuth } from "../context/AuthContext";
 
 const RecordActivity = () => {
-  const [locations, setLocations] = useState([]);
-
   const [error, setError] = useState("");
 
   const auth = useAuth();
   const api = useApi(auth);
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    api.getLocations().then((data) => {
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setLocations(data.data);
-      }
-    });
-  }, [api]);
 
   const handleSave = async ({
     title,
@@ -43,12 +31,7 @@ const RecordActivity = () => {
     <div className="relative mx-auto h-full max-w-xl p-4">
       <h1 className="pt-5 text-left text-3xl">Record Activity</h1>
       {error != "" && <span className="text-red-500">{error}</span>}
-      <ActivityForm
-        handleSave={handleSave}
-        locations={locations}
-        setError={setError}
-        api={api}
-      />
+      <ActivityForm handleSave={handleSave} setError={setError} api={api} />
     </div>
   );
 };
