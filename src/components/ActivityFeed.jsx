6f0dee1070f6f78961daa4ec4487f32c0aedd46f
Robert Pettit <robert.pettit@sky.uk>
2024-08-14 19:06:12 +0100

    finished joined friends and delete activity

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -8,32 +8,49 @@ const ActivityFeed = () => {
   const [activities, setActivities] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [onlyYou, setOnlyYou] = useState(false);
+
+  async function getData(urlEnding) {
+    try {
+      const response = await axios.get(`${DATABASE_URL}/activities/${urlEnding}`);
+      if (response.status != 200) {
+        console.log(response);
+        throw new Error("Network response was not ok");
+      }
+      setActivities(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  }
+
 
   useEffect(() => {
     const fetchActivities = async () => {
-      try {
-        const response = await axios.get(`${DATABASE_URL}/activities/getAll`);
-        if (response.status != 200) {
-          console.log(response);
-          throw new Error("Network response was not ok");
-        }
-        setActivities(response.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
+      getData('getAll');
     };
 
     fetchActivities();
   }, []);
+
+  const changeOnlyYou = async () => {
+    if(onlyYou){
+      setOnlyYou(false);
+      getData('getAll');
+    }
+    else{
+      setOnlyYou(true);
+      //Will change this to which user is signed in, or even better the backend will know which user is logged in so no need to pass in userId
+      getData('getUserActivities?userId=1')
+    }
+  }
 
   if (loading) {
     return <div className="mx-auto max-w-4xl p-4">Loading...</div>;
   }
 
   if (error) {
-    console.log(error);
     return <div className="mx-auto max-w-4xl p-4">Error: {error.message}</div>;
   }
 
@@ -44,7 +61,7 @@ const ActivityFeed = () => {
         <div className='h-[10%] text-right'>
           <label className="inline-flex items-center cursor-pointer">
             <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-500">Only you</span>
-            <input type="checkbox" value="" className="sr-only peer"></input>
+            <input type="checkbox" value="" className="sr-only peer" onChange={changeOnlyYou}></input>
             <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
           </label>
         </div>
